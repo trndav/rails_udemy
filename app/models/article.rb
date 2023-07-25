20ae07ba5cc3074f750858de074a55e17c290cbe
trndav <farsiteguide@gmail.com>
2023-07-25 15:12:07 +0200

    "Added articles controller, models and view action"

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
-# class Articje inherits from application_record.rb
+# class Article inherits from application_record.rb
+# Here we add constraints for article: title and desc must be present
+# Title/desc length is set
 
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+#Configuring routes/pages
+
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
+  resources :articles, only: [:show]
 end
