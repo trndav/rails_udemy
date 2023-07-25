74fb4a0aebccc21ecd5389434870aad59482c6b5
trndav <farsiteguide@gmail.com>
2023-07-25 16:36:59 +0200

    "Article listing page. Added :index in routes.rb, added index method in articles_controller.rb, added index.thml.erb in app/articles/views with ruby codes to display articles."

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
+    def index
+        @articles5 = Article.all
+    end
 end
