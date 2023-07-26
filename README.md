8902f50894caa6b4e76b7d1f66d9ca8844141822
trndav <farsiteguide@gmail.com>
2023-07-26 17:58:09 +0200

    "Update existing article functionality."

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
 # README
 
-Working with Udemy Rails. Test repository for GitHub. Another change.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Working with Udemy Rails. Test repository for GitHub. 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -16,6 +19,16 @@ class ArticlesController < ApplicationController
         else
             render "new"
             #render :new, status: :unprocessable_entity
+        end
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            #render "edit"
+            render :edit, status: :unprocessable_entity
         end
     end
 end
